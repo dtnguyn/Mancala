0561a0154c33f4c6d665572a31908fba0a3a4170
Dzung Nguyen <adron0914@gmail.com>
2019-09-19 22:28:32 +0700

    mancala 1.0.0

---
+++ src/mancala/pkg1/pkg0/Player.java
@@ -0,0 +1,125 @@
+
+package mancala.pkg1.pkg0;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    public  List <Slot> slots;
+    private Player opponent;
+    private Boolean bonusTurn;
+    private Boolean endGame;
+
+    public Player(){
+        this.slots = new ArrayList();
+        this.bonusTurn = false;
+        this.endGame = false;
+        initializeSlots();
+        this.slots.get(6).cleanStones();
+    }
+    
+    public void initializeSlots(){
+        for(int i = 0; i < 7; i++){
+            Slot slot = new Slot();
+            this.slots.add(slot);
+        }
+    }
+    
+//    public void addSlot(Slot slot){
+//        this.slots.add(slot);
+//    }
+       
+    public void setOpponent(Player opponent){
+        this.opponent = opponent;
+    }
+
+    public Boolean getEndGame() {
+        return endGame;
+    }
+
+    public void addStoneToSlot(int indexSlot, Stone stone){
+        this.slots.get(indexSlot).addStone(stone);
+    }
+    
+    public void moveStonesAround(int position, Slot slot){
+        int opponentPosition = 0;
+        Slot tempSlot = new Slot();
+        tempSlot.cleanStones();
+        for(int i = 0; i < slot.countStones(); i++){
+            Stone stone = new Stone();
+            tempSlot.addStone(stone);
+        }
+        if(this.slots.get(position).countStones() != 0){
+            slot.cleanStones();
+            for (int i = 0; i < tempSlot.countStones(); i++){
+                if(position < 6){
+                    position++;
+                    addStoneToSlot(position, tempSlot.getStoneByIndex(i));
+                    setBonusTurn(i, tempSlot.countStones(), position);
+                } else if(position == 6) {
+                    if(opponentPosition < 6){
+                        opponent.addStoneToSlot(opponentPosition, tempSlot.getStoneByIndex(i));
+                        opponentPosition++;
+                    } else if(opponentPosition == 6) {
+                        position = 0;
+                        addStoneToSlot(position, tempSlot.getStoneByIndex(i));
+                    }    
+                }
+                
+            }
+            winningStones(position);
+            isEndGame();
+            
+
+            
+        } else bonusTurn = true;
+
+    }
+    
+    
+    public void isEndGame(){
+        int totalStones1 = 0;
+        int totalStones2 = 0;
+        
+        for(int i = 0; i < 6; i++){
+            totalStones1 += slots.get(i).countStones();
+            totalStones2 += this.opponent.slots.get(i).countStones();
+        }
+        
+        if(totalStones1 == 0 || totalStones2 == 0){
+            this.endGame = true;
+        }else this.endGame = false;
+        
+    }
+    
+    
+    public void winningStones(int position){
+        if (position != 6 && this.slots.get(position).countStones() == 1
+            && this.opponent.slots.get(5-position).countStones() != 0){
+                addStoneToSlot(6, this.slots.get(position).getStoneByIndex(0));
+                takingStonesFromOpponent(position);
+                this.slots.get(position).cleanStones();
+                this.opponent.slots.get(5-position).cleanStones();
+        } 
+    }
+    
+    public  void setBonusTurn(int index, int count, int position){
+        isEndGame();
+        if(!getEndGame()){
+            if(index == count - 1 && position == 6){
+                this.bonusTurn = true;
+            }else this.bonusTurn = false; 
+        }
+
+    }
+
+    public Boolean getBonusTurn() {
+        return this.bonusTurn;
+    }
+    
+    public void takingStonesFromOpponent(int position){
+        for(int j = 0; j < this.opponent.slots.get(5-position).countStones(); j++ ){
+            addStoneToSlot(6, this.opponent.slots.get(5-position).getStoneByIndex(j));
+        }
+    }
+}
